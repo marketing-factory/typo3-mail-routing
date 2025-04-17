fb7461d596f93a414aaf84e58d34e1d1d0ddcb17
Christian Spoo <christian.spoo@marketing-factory.de>
2025-04-17 14:13:19 +0200

    [TASK] Add ext_emconf.php and improve description

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 mail routing',
+    'description' => 'A TYPO3 CMS extension that enables flexible e-mail transport routing. This extension allows you to control which mail transport is used for sending emails either globally for your entire site or on a per-email basis.',
+    'category' => 'services',
+    'author' => 'Christian Spoo',
+    'author_email' => 'christian.spoo@marketing-factory.de',
+    'author_company' => 'Marketing Factory Digital GmbH',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.5-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Mfd\\Mail\\Routing\\' => 'Classes/',
+        ],
+    ],
+]; 
