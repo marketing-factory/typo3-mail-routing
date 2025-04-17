d981d383ae9e9c2523ce7bb0d4d48816a7f7f3c5
Christian Spoo <christian.spoo@marketing-factory.de>
2025-04-17 15:23:29 +0200

    [TASK] Set version to 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['mail_routing'] = [
     'title' => 'TYPO3 mail routing',
     'description' => 'A TYPO3 CMS extension that enables flexible e-mail transport routing. This extension allows you to control which mail transport is used for sending emails either globally for your entire site or on a per-email basis.',
     'category' => 'services',
     'author' => 'Christian Spoo',
     'author_email' => 'christian.spoo@marketing-factory.de',
     'author_company' => 'Marketing Factory Digital GmbH',
-    'state' => 'stable',
-    'version' => '1.0.0',
+    'state' => 'beta',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.5-13.4.99',
@@ -21,4 +21,4 @@ $EM_CONF[$_EXTKEY] = [
             'Mfd\\Mail\\Routing\\' => 'Classes/',
         ],
     ],
-]; 
+];
